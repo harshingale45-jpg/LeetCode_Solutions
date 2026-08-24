375ca89cbe344b84dd46a3272c0189935ea90ec7
Harshvardhan Ingale <harshingale45@gmail.com>
2026-08-24 22:10:41 +0530

    Time: 3 ms (100%), Space: 83.3 MB (84.26%) - LeetHub

---
+++ 1872-stone-game-viii/1872-stone-game-viii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int stoneGameVIII(int[] stones) {
+        int n = stones.length;
+
+        // Prefix sums
+        for (int i = 1; i < n; i++) {
+            stones[i] += stones[i - 1];
+        }
+
+        int best = stones[n - 1];
+
+        // DP from right to left
+        for (int i = n - 2; i >= 1; i--) {
+            best = Math.max(best, stones[i] - best);
+        }
+
+        return best;
+    }
+}
